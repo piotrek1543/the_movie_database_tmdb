468e10fd3b34cff924d8c241fc4c9fe8f3d3dbd4
piotrek1543 <piotrekert90@gmail.com>
2025-03-12 21:39:36 +0100

    Add Coil image loading library configuration.

--- app/src/main/java/com/example/tmdb/TMDBApplication.kt
+++ app/src/main/java/com/example/tmdb/TMDBApplication.kt
@@ -1,11 +1,17 @@
 package com.example.tmdb
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
 import com.example.tmdb.data.AppContainer
 import com.example.tmdb.data.DefaultAppContainer
 import timber.log.Timber
 
-class TMDBApplication : Application() {
+class TMDBApplication : Application(), ImageLoaderFactory {
 
     lateinit var container: AppContainer
 
@@ -15,5 +21,25 @@ class TMDBApplication : Application() {
             Timber.plant(Timber.DebugTree())
         }
         container = DefaultAppContainer()
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader(this)
+            .newBuilder()
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .build()
     }
 }
