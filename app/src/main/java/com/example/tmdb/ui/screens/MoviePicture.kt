09ef5e560bf589486d8bf24ab89171659a484a0d
piotrek1543 <piotrekert90@gmail.com>
2025-03-15 21:42:59 +0100

    Refactor: Update MoviePicture composable imports.

--- app/src/main/java/com/example/tmdb/ui/screens/MoviePicture.kt
+++ app/src/main/java/com/example/tmdb/ui/screens/MoviePicture.kt
@@ -1,7 +1,6 @@
 package com.example.tmdb.ui.screens
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -10,7 +9,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.tmdb.R
