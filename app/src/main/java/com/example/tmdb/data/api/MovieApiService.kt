445b676c247f4cd5958183f8e2c53e2100c41a6e
piotrek1543 <piotrekert90@gmail.com>
2025-03-15 20:26:48 +0100

    Refactor: Update getNowPlayingMovies to take language, page, and region parameters.
    - Update `getNowPlayingMovies` in `MovieApiService` to accept `language`, `page`, and `region` parameters.
    - Set default values for `language` to "en-US" and `page` to 1.
    - Make `region` optional.

--- app/src/main/java/com/example/tmdb/data/api/MovieApiService.kt
+++ app/src/main/java/com/example/tmdb/data/api/MovieApiService.kt
@@ -5,8 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-
-
 /**
  * Interface for interacting with the Movie API.
  */
@@ -16,6 +14,9 @@ interface MovieApiService {
      * Retrieves a list of movies currently playing in theaters.
      *
      * @param apiKey The API key for authentication. It's a mandatory parameter.
+     * @param language The language to return the movies in. Default is "en-US"
+     * @param page The page number to retrieve. Default is 1
+     * @param region The region to return the movies from. Optional.
      * @return A [NowPlayingMovieResponse] object containing the list of movies.
      * @throws HttpException If the API request fails (e.g., 401 Unauthorized, 404 Not Found).
      * @throws IOException If there's a network-related issue.
@@ -23,6 +24,9 @@ interface MovieApiService {
      */
     @GET("movie/now_playing")
     suspend fun getNowPlayingMovies(
-        @Query("api_key") apiKey: String
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String = "en-US",
+        @Query("page") page: Int = 1,
+        @Query("region") region: String? = null,
     ): Response<NowPlayingMovieResponse>
 }
