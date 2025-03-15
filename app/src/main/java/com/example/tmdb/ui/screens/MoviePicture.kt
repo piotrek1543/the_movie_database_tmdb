e520ee48c3488919b510bd9481713ae5a1381dbf
piotrek1543 <piotrekert90@gmail.com>
2025-03-15 21:18:12 +0100

    Add MoviePicture preview.

--- app/src/main/java/com/example/tmdb/ui/screens/MoviePicture.kt
+++ app/src/main/java/com/example/tmdb/ui/screens/MoviePicture.kt
@@ -1,6 +1,7 @@
 package com.example.tmdb.ui.screens
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -8,10 +9,13 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.tmdb.R
 import com.example.tmdb.data.api.TmdbApi
+import com.example.tmdb.ui.theme.TMDBTheme
 
 @Composable
 fun MoviePicture(imagePath: String?) {
@@ -52,4 +56,12 @@ fun MoviePicture(imagePath: String?) {
         contentScale = ContentScale.Crop,
         modifier = Modifier.fillMaxWidth()
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MoviePicturePreview() {
+    TMDBTheme {
+        MoviePicture(imagePath = "/7iMBZzVZtG0oBug4TfqDb9ZxAOa.jpg")
+    }
 }
