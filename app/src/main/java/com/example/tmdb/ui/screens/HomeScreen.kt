502c2cefa2de38212cfcf8ce223a03097b534678
piotrek1543 <piotrekert90@gmail.com>
2025-03-15 21:49:12 +0100

    Refactor: Update HomeScreen layout and states.
    - Update `HomeScreen` to use `Box` for layout.
    - Update `HomeScreen` states to use `LoadingScreen`, `MovieGridScreen`, and `ErrorScreen`.
    - Update `HomeScreen` to use `contentPadding`.

--- app/src/main/java/com/example/tmdb/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/tmdb/ui/screens/HomeScreen.kt
@@ -1,9 +1,12 @@
 package com.example.tmdb.ui.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.tmdb.MoviesViewModel
@@ -15,24 +18,20 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(0.dp),
 ) {
-    when (uiState) {
-        is MoviesViewModel.MoviesUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
-        is MoviesViewModel.MoviesUiState.Success -> MovieGridScreen(
-            uiState.movies,
-            contentPadding = contentPadding,
-            modifier = modifier.fillMaxWidth()
-        )
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(contentPadding),
+        contentAlignment = Alignment.Center
+    ) {
+        when (uiState) {
+            is MoviesViewModel.MoviesUiState.Loading -> LoadingScreen()
+            is MoviesViewModel.MoviesUiState.Success -> MovieGridScreen(
+                movies = uiState.movies,
+                modifier = Modifier.fillMaxWidth()
+            )
 
-        is MoviesViewModel.MoviesUiState.Error -> ErrorScreen(
-            retryAction,
-            modifier = modifier.fillMaxSize()
-        )
+            is MoviesViewModel.MoviesUiState.Error -> ErrorScreen(retryAction = retryAction)
+        }
     }
 }
-
-
-
-
-
-
-
